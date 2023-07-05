243147d34a1e2fe8029e14a7456a36f255cb20b6
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-07-05 16:49:20 +0300

    Nav bar

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
-        
-        .then(response =>response.json())
-        .then(alcoholic_cocktails => console.log(alcoholic_cocktails))
+const form = document.querySelector("form").addEventListener('submit', fetchUserInput)
+const userInput = document.querySelector("input")
+
+
+function fetchUserInput (){
+
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userInput}`)       
+.then(response =>response.json())
+.then(cocktailsByName => console.log(cocktailsByName))
+
+}
            
